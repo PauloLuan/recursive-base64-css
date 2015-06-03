5b77dbaf441a38efdfef3913fea1cd66d46f047f
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-03 10:46:47 -0300

    added some assertions for initial options

--- index.js
+++ index.js
@@ -6,6 +6,18 @@
  * Licensed under the MIT license.
  */
 
+var assert = require("assert")
+var path = require("path")
+var fs = require("fs")
+var glob = require("glob")
+
 module.exports = {
-    
+	init: function(destinationPath, options) {
+		options = options || {};
+
+		assert(destinationPath, 'base64: missing path');
+		assert.equal(typeof destinationPath, 'string', 'base64: path should be a string');
+		assert(options, 'base64: missing options');
+		assert.equal(typeof options, 'object', 'base64: options should be object');
+	}
 };
