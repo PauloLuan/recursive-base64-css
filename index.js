68fcaefa39156e11ccba6762c04d8377c04cc73c
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-03 10:12:27 -0300

    created index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Recursive CSS base64 inline
+ * https://github.com/PauloLuan/recursive-base64-css
+ *
+ * Copyleft (c) 2015 Paulo Luan
+ * Licensed under the MIT license.
+ */
+
+module.exports = {
+    
+};
