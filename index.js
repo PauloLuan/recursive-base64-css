05aa6b8fecdffad9dc33df037241bfb4d88db69f
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-03 10:59:29 -0300

    added glob funtion to get all css files

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ var fs = require("fs")
 var glob = require("glob")
 
 module.exports = {
-	init: function(destinationPath, options) {
-		options = options || {};
+    init: function (destinationPath, options) {
+        options = options || {};
 
-		assert(destinationPath, 'base64: missing path');
-		assert.equal(typeof destinationPath, 'string', 'base64: path should be a string');
-		assert(options, 'base64: missing options');
-		assert.equal(typeof options, 'object', 'base64: options should be object');
-	}
+        assert(destinationPath, 'base64: missing path');
+        assert.equal(typeof destinationPath, 'string', 'base64: path should be a string');
+        assert(options, 'base64: missing options');
+        assert.equal(typeof options, 'object', 'base64: options should be object');
+    },
+
+    getAllCssFiles: function (destinationPath, callback) {
+        var destPath = path.join(destinationPath + "/*.css");
+        glob(destPath, function (er, files) {
+            if (err) {
+                console.log("Error: ", err);
+            } else {
+                callback(files);
+            }
+        });
+    }
 };
