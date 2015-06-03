7ff4ea15aa5da89c49521354b7ab28c4e3d8f9a2
Paulo Luan <pauloluan.inova@gmail.com>
2015-06-03 10:12:09 -0300

    added test initial structure

---
+++ test/index.js
@@ -0,0 +1,8 @@
+var should = require('chai').should(),
+    module = require('../index');
+
+describe('function name', function () {
+    it('should introduce something here', function () {
+        
+    });
+});
